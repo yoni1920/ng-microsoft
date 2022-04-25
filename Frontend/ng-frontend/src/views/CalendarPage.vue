5b1eeaeaeee345247d2dfe5d145e82156f1954ce
yoni1920 <yoni.81416@gmail.com>
2022-04-25 11:26:15 +0300

    added calendar

---
+++ Frontend/ng-frontend/src/views/CalendarPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="calendar">
+    <Calendar/>
+  </v-container>
+</template>
+
+<script>
+  import Calendar from '../components/Calendar'
+
+  export default {
+    name: 'CalendarPage',
+
+    components: {
+      Calendar,
+    },
+  }
+</script>
--- Frontend/ng-frontend/src/views/Home.vue
+++ Frontend/ng-frontend/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
+  <v-container class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </v-container>
 </template>
 
 <script>
