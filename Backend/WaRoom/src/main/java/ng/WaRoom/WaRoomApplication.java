a8ed6c90ce411661170200efd0844b7d98d054dd
Idoozerii <idoozeri7@gmail.com>
2022-04-25 11:00:37 +0300

    init server project using spring

---
+++ Backend/WaRoom/src/main/java/ng/WaRoom/WaRoomApplication.java
@@ -0,0 +1,13 @@
+package ng.WaRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaRoomApplication.class, args);
+	}
+
+}
---
+++ Backend/WaRoom/src/test/java/ng/WaRoom/WaRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.WaRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
